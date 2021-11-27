4e2d4898696ae3f2ef44a8efbf2e763ba7015c85
AcruxTech <78689853+AcruxTech@users.noreply.github.com>
2021-11-27 16:30:15 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <!-- <HelloWorld msg="123"/> -->
+    <Weather> </Weather>
+  </div>
+</template>
+
+<script>
+import Weather from "./components/Weather.vue";
+
+export default {
+  name: "App",
+  components: {
+    Weather,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
